107c40307c17fbb467b297fc5cc5c1abf82682fb
hellowinter2025 <hellowinter2025@qq.com>
2026-09-05 23:06:57 +0800

    style: use open source serif and sans Chinese fonts

--- tools/build-font-data.js
+++ tools/build-font-data.js
@@ -2,9 +2,10 @@ const fs = require('fs');
 const path = require('path');
 
 const root = path.resolve(__dirname, '..');
-const fonts = Object.fromEntries(['boyang','huakang','xingkai','tianying','anjing','xinwei'].map(key => [key, 'NotoSansSC-VF.ttf']));
-const noto = fs.readFileSync(path.join(root, 'assets', 'fonts', 'NotoSansSC-VF.ttf')).toString('base64');
+const fonts = {boyang:'NotoSerifSC-VF.ttf',huakang:'NotoSansSC-VF.ttf',xingkai:'NotoSerifSC-VF.ttf',tianying:'NotoSerifSC-VF.ttf',anjing:'NotoSerifSC-VF.ttf',xinwei:'NotoSerifSC-VF.ttf'};
+const sans = fs.readFileSync(path.join(root,'assets','fonts','NotoSansSC-VF.ttf')).toString('base64');
+const serif = fs.readFileSync(path.join(root,'assets','fonts','NotoSerifSC-VF.ttf')).toString('base64');
 fs.writeFileSync(
   path.join(root, 'assets', 'fonts', 'font-data.js'),
-  `window.TIMETABLE_FONT_DATA=(()=>{const n=${JSON.stringify(noto)};return {boyang:n,huakang:n,xingkai:n,tianying:n,anjing:n,xinwei:n};})();\n`,
+  `window.TIMETABLE_FONT_DATA=(()=>{const s=${JSON.stringify(sans)},r=${JSON.stringify(serif)};return {boyang:r,huakang:s,xingkai:r,tianying:r,anjing:r,xinwei:r};})();\n`,
 );
